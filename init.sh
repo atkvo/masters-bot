92ef060849223e3d8020b666d1620b4ed6fd5d60
Andrew Vo <voandrew.khoa@gmail.com>
2017-03-20 05:44:22 +0000

    Add package via catkin_create_package instead of roscreate_pkg

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 cd src
 catkin_init_workspace
 cd ..
+rosdep install autobot # installs dependencies required by autobot package
 catkin_make
 # source devel/setup.bash ## doesn't seem to work here. call this manually
 echo "Initialization is done"
