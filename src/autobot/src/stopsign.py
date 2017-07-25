9066d3e5bdbc95fb347b1a081d9b7db33ab68ea4
Andrew Vo <voandrew.khoa@gmail.com>
2017-07-24 17:50:44 -0700

    Fix state machine using wrong variable

--- src/autobot/src/stopsign.py
+++ src/autobot/src/stopsign.py
@@ -14,7 +14,7 @@ class StopSign(object):
     def __init__(self):
         self.state = StopStates.NORMAL
         self.stopDuration = 2
-        self.ignoreDuration = 2
+        self.ignoreDuration = 4
 
     def stopSignDetected(self):
         self.state = StopStates.FULL_STOP
@@ -22,15 +22,15 @@ class StopSign(object):
                             self.stepStateMachine,
                             oneshot=True)
 
-    def stepStateMachine(self):
+    def stepStateMachine(self, event):
         if self.state is StopStates.NORMAL:
-            self.action = StopStates.FULL_STOP
+            self.state = StopStates.FULL_STOP
 
         elif self.state is StopStates.FULL_STOP:
-            self.action = StopStates.IGNORE_STOP_SIGNS
+            self.state = StopStates.IGNORE_STOP_SIGNS
             timer = rospy.Timer(rospy.Duration(self.ignoreDuration),
                                 self.stepStateMachine,
                                 oneshot=True)
 
         elif self.state is StopStates.IGNORE_STOP_SIGNS:
-            self.action = StopStates.NORMAL
+            self.state = StopStates.NORMAL
